48f68152a697fcdc81143bddbbcbcb4309018753
Fluke <jesadakorn.kirtnu@gmail.com>
2021-03-15 14:34:05 +0700

    add google map ios + android

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyCMeNAFb3ZQa567IwXaJUy13kAmIu7noeA")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
